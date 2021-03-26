cd8468f0175bb509e9f92b4ead7a56e2ff1231fe
silfigabrilia <66557176+silfigabrilia@users.noreply.github.com>
2021-03-26 12:12:03 +0700

    Activity Login

---
+++ E31191849_SilfiGabrilia/app/src/main/java/com/example/e31191849_silfigabrilia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.e31191849_silfigabrilia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
